a7146a2fb7261e735cd8adca04acf296857c8e3a
Sebastian Guerrero <seba.guerrero.f@gmail.com>
2021-05-05 00:00:19 -0400

    Ultima configuración

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
+app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
 
-app.use(express.static('./dist/<name-on-package.json>'));
-
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-  );
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
